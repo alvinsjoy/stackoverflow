df8c5dc8b3a6406672a9b8bb84773b445aa2374a
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-10-08 19:32:04 +0530

    Add Prettier, collection definitions

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+
+import getOrCreateDB from './models/server/dbSetup';
+import getOrCreateStorage from './models/server/storageSetup';
+
+// This function can be marked `async` if using `await` inside
+export async function middleware() {
+  await Promise.all([getOrCreateDB(), getOrCreateStorage()]);
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
