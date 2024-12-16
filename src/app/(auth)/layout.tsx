8204b3a42c0c3062102c15cbb3230b99fc8bf57a
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-12-16 23:39:39 +0530

    refactor layout structure and add answer API for managing answers and reputation

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,19 +1,14 @@
 'use client';
 
 import { BackgroundBeams } from '@/components/ui/background-beams';
-import Providers from '../provider';
 import React from 'react';
-import ThemeSwitch from '@/components/theme-switch';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Providers>
-      <div className="relative flex min-h-screen flex-col items-center justify-center py-12">
-        <ThemeSwitch />
-        <BackgroundBeams />
-        <div className="relative">{children}</div>
-      </div>
-    </Providers>
+    <div className="relative flex min-h-screen flex-col items-center justify-center py-12">
+      <BackgroundBeams />
+      <div className="relative">{children}</div>
+    </div>
   );
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
+import Providers from './provider';
+import ThemeSwitch from '@/components/theme-switch';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -24,11 +26,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          <ThemeSwitch />
+          {children}
+        </Providers>
       </body>
     </html>
   );
